6c2b2e6017725faca2957c465ed913d0caf97b76
Johnny Huynh <johnnyhuynh28@yahoo.com>
2020-12-18 22:04:05 -0500

    implemented student.h

---
+++ student.cpp
@@ -0,0 +1,9 @@
+#include "Student.h"
+
+Student::Student(){
+    this->studentID = "";
+    this->firstName = "";
+    this->lastName = "";
+    this->email = "";
+    this->age = 0;
+}
--- student.h
+++ student.h
@@ -1,9 +1,41 @@
 #pragma once
 #include <iostream>
-#include "degree.h"
+#include "Degree.h"
 using std::string;
 using std::cout;
 
-class student{
-    
-}
+class Student {
+    public:
+        const static int numberOfDays = 3;
+    private:
+        string studentID;
+        string firstName;
+        string lastName;
+        string email;
+        int age;
+        int days[numberOfDays];
+        Degree degree;
+    public:
+        Student();
+        Student(string studentID, string firstName, string lastName, string email, int age, int days[], Degree degree);
+        ~Student();
+
+        string getID();
+        string getFirstName();
+        string getLastName();
+        string getEmail();
+        int getAge();
+        int* getDays();
+        Degree getDegree();
+
+        void setID(string studentID);
+        void setFirstName(string firstName);
+        void setLastName(string lastName);
+        void setEmail(string email);
+        void setAge (int age);
+        void setDays(int days[]);
+        void setDegree(Degree degree);
+
+        void print();
+
+};
